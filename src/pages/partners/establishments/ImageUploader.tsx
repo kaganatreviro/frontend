4e1b7ff8d474e58643d1239884576f7ab08c6c89
Camila <cafieness@gmail.com>
2024-05-14 19:54:24 +0600

    updated Image uploader

--- src/pages/partners/establishments/ImageUploader.tsx
+++ src/pages/partners/establishments/ImageUploader.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable */
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faImage, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 interface ImageUploaderProps {
   onUpload: (file: File) => void;
@@ -12,7 +14,7 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ onUpload }) => {
     const file = event.target.files && event.target.files[0];
     if (file) {
       setImage(file);
-      onUpload(file); // Call the onUpload callback with the uploaded file
+      onUpload(file);
     }
   };
 
@@ -21,16 +23,39 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ onUpload }) => {
   };
 
   return (
-    <div>
+    <div className="w-[200px]">
       {image ? (
-        <div>
-          <img src={URL.createObjectURL(image)} alt="Uploaded Image" />
-          <button onClick={handleImageRemove}>Remove Image</button>
+        <div className="relative flex">
+          <img
+            src={URL.createObjectURL(image)}
+            alt="Uploaded Image"
+            className="w-[200px] h-[200px] rounded-full"
+          />
+          <button
+            className="absolute top-0 right-0  w-10 h-10 bg-white border border-[#FB7E00] rounded-full flex items-center justify-center -mt-2 -mr-2 focus:outline-none"
+            onClick={handleImageRemove}
+          >
+            <FontAwesomeIcon
+              icon={faTimes}
+              className="w-6 h-6 text-[#FB7E00]"
+            />
+          </button>
         </div>
       ) : (
-        <div>
-          <input type="file" accept="image/*" onChange={handleImageChange} />
-        </div>
+        <label
+          htmlFor="upload-input"
+          className="w-[200px] h-[200px] rounded-md cursor-pointer text-lg text-[#FB7E00] flex flex-col justify-center items-center border-2 border-dashed border-[#FB7E00]"
+        >
+          <FontAwesomeIcon icon={faImage} className="w-20 h-20 mb-4" />
+          <div className="text-center">Click to upload your image</div>
+          <input
+            id="upload-input"
+            type="file"
+            accept="image/*"
+            style={{ display: "none" }}
+            onChange={handleImageChange}
+          />
+        </label>
       )}
     </div>
   );
